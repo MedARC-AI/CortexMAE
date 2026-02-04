5fa41fc67ac007c8644badccff5fbbb749b7f263
Connor Lane <connor.lane858@gmail.com>
2026-02-04 00:03:48 +0000

    add exp scripts for input space v2 and input norm

---
+++ experiments/input_norm/launch_pretrain.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=input_norm
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --time=infinite
+#SBATCH --partition=main
+#SBATCH --output=slurms/slurm-%A_%a.out
+#SBATCH --nodelist=n-1,n-2
+#SBATCH --account=training
+#SBATCH --array=0-4
+
+set -euo pipefail
+
+ROOT="${HOME}/fmri-fm"
+cd $ROOT
+
+# export all env variables
+set -a
+source .env
+set +a
+
+EXP_NAME="input_norm"
+EXP_DIR="experiments/${EXP_NAME}"
+OUT_DIR="${EXP_DIR}/output"
+
+configs=(
+    "coord_frame|no_coord_normalize=false normalize=frame"
+    "coord_global|no_coord_normalize=false normalize=global"
+    "coord_none|no_coord_normalize=false normalize=null"
+    "nocoord_frame|no_coord_normalize=true normalize=frame"
+    "nocoord_global|no_coord_normalize=true normalize=global"
+)
+
+config=${configs[SLURM_ARRAY_TASK_ID]}
+name=$(echo $config | cut -d '|' -f 1)
+overrides=$(echo $config | cut -d '|' -f 2)
+
+base_config="${EXP_DIR}/pretrain.yaml"
+fullname="${EXP_NAME}/${name}/pretrain"
+notes="input_norm ablations $name (${overrides})"
+
+# add small delay between jobs
+# bit of hack to try to get wandb to assign different colors
+sleep $(( SLURM_ARRAY_TASK_ID * 10 ))
+
+# for debugging
+# overrides="${overrides} debug=true wandb=false"
+
+uv run torchrun --standalone --nproc_per_node=1 \
+    src/flat_mae/main_pretrain.py \
+    --cfg-path "${base_config}" \
+    --overrides \
+    $overrides \
+    name="${fullname}" \
+    notes="${notes}" \
+    output_dir="${OUT_DIR}"
